220b883a8f5118e0a2d6565f98c34f0082ac50de
Therge Fonseca <thergefonseca@gmail.com>
2022-11-28 01:25:10 -0300

    createuser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
+import createUser from './endpoints/createUser'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+app.put("/user", createUser)
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if(server){
